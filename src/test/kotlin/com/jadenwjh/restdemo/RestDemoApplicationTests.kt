a5f896b11fde7bccb2a8b0229e65853dcc62f5c4
Jaden Wong <jaden.wong@endowus.com>
2022-02-01 16:46:46 +0800

    [task] first commit

---
+++ src/test/kotlin/com/jadenwjh/restdemo/RestDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jadenwjh.restdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
